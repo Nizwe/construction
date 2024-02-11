126151f656deacfd349b3bf92ac8142c1e9d2f4b
nizwe <nicolas.tabellion@gmail.com>
2024-02-11 15:15:59 +0100

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/under_construction.png" className="App-logo" alt="logo" />
+        <p>
+         Weizenbier werden eingeschenkt
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
